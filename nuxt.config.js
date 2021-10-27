e74be57cbfd100b042dde94ce491bd2d8fec32a0
naveenperera512 <naveenperera512@gmail.com>
2021-10-27 18:27:27 +0530

    build

---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'moxy-base-fe',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://unpkg.com/@tabler/core@latest/dist/css/tabler.min.css"
+      },
+    ],
+    script: [
+      {
+        src: "https://unpkg.com/@tabler/core@latest/dist/js/tabler.min.js",
+        async: true,
+        body:false,
+      },
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    'nuxt-lazy-load',
+    '@nuxtjs/toast',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
