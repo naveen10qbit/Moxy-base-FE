b0a624e8cf98c875b91f6af62f154aaff5af155e
Nisitha Dayarathna <nisitha56190@gmail.com>
2021-10-28 16:25:02 +0530

    Minor changes added

--- components/Jobs/SingleJob.vue
+++ components/Jobs/SingleJob.vue
@@ -9,7 +9,7 @@
       <h4 class="font-monospace mt-3 ms-2">Lahiru Prasad</h4>
     </div>
     <!-- <hr /> -->
-    <h2 class="font-monospace mt-3">Looking for mentors...</h2>
+    <h2 class="mt-3">Looking for mentors...</h2>
     <h4>Description</h4>
     <h5>This is description 1</h5>
   </div>
